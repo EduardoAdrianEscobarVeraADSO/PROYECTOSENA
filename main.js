78f3fea82a71b3b363f2182449f80fd29a6136ca
EduardoAdrianEscobarVeraADSO <eduardoadrianescobar12@gmail.com>
2024-04-04 16:42:44 -0500

    carrusel

---
+++ main.js
@@ -0,0 +1,17 @@
+document.getElementById('prevBtn').addEventListener('click', function() {
+    const sliderContainer = document.querySelector('.slider-container');
+    const scrollAmount = sliderContainer.scrollLeft - sliderContainer.clientWidth;
+    sliderContainer.scrollTo({
+        left: scrollAmount,
+        behavior: 'smooth'
+    });
+});
+
+document.getElementById('nextBtn').addEventListener('click', function() {
+    const sliderContainer = document.querySelector('.slider-container');
+    const scrollAmount = sliderContainer.scrollLeft + sliderContainer.clientWidth;
+    sliderContainer.scrollTo({
+        left: scrollAmount,
+        behavior: 'smooth'
+    });
+});
