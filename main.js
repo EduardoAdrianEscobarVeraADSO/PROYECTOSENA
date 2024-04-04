063542b4c5a1e52357bb53b428846f351b374622
EduardoAdrianEscobarVeraADSO <eduardoadrianescobar12@gmail.com>
2024-04-04 17:26:28 -0500

    cinta led

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-document.getElementById('prevBtn').addEventListener('click', function() {
+// codigo carrusel de imagenes
+
+document.getElementById('prevBtn').addEventListener('click', function () {
     const sliderContainer = document.querySelector('.slider-container');
     const scrollAmount = sliderContainer.scrollLeft - sliderContainer.clientWidth;
     sliderContainer.scrollTo({
