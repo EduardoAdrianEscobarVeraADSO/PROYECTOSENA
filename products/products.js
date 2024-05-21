15fe2b1fd5f110969bbdfce0760715beb870607c
EduardoAdrianEscobarVeraADSO <eduardoadrianescobar12@gmail.com>
2024-05-21 10:10:17 -0500

    avance

--- products/products.js
+++ products/products.js
@@ -1,4 +1,3 @@
-
 // Función para abrir/cerrar el menú
 function toggleMenu() {
     let menu = document.getElementById("myNav");
@@ -8,31 +7,7 @@ function toggleMenu() {
       menu.style.width = '250px';
     }
 }
-  
-
-  
-// Función para ejecutar animacion scroll header superior
-
-  window.addEventListener('scroll', function() {
-    let header = document.querySelector('.header');
-    let menuIcon = document.querySelector('.header__menu-icon');
-    let logoImage = document.getElementById('logoImage');
-    let headerItems = document.getElementById('header__items').children; // Selecciona todos los ítems dentro del div con ID 'header__items'
-  
-    if (window.scrollY > 10) {
-      header.style.backgroundColor = 'white';
-      menuIcon.style.color = 'black';
-      logoImage.src = 'img/logo.png'; 
-      Array.from(headerItems).forEach(item => item.style.color = 'black'); // Cambia el color de todos los ítems a negro
-    } else {
-      header.style.backgroundColor = 'transparent';
-      menuIcon.style.color = 'white';
-      logoImage.src = 'img/logoh2.png'; 
-      Array.from(headerItems).forEach(item => item.style.color = 'white'); 
-    }
-  });
-
-  // Guarda los dos títulos alternativos
+// Guarda los dos títulos alternativos
 const title1 = '🔥🔥🔥 ¡Regresa! 🔥🔥🔥' ;
 const title2 = '¡Vuelve aqui!';
 const originalTitle = document.title;
