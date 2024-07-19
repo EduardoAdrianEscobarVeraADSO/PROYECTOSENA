e626efa5726a24c1cf92ed3236c4ab16860ecd14
EduardoAdrianEscobarVeraADSO <eduardoadrianescobar12@gmail.com>
2024-07-19 14:05:15 -0500

    avance

--- JS/login.js
+++ JS/login.js
@@ -87,45 +87,6 @@ const validarCorreo = (elemento) => {
     }
 };
 
-// Función para validar el numero telefonico
-const validarNumeros = (elemento) => {
-    
-    const spanId = `spanError${elemento.id}`;
-    const errorMessage = "Numero invalido, debe tener 10 caracteres como minimo y como maximo";
-
-    // Expresion regular 
-    const regexNumero = /^\d{10}$/;
-
-  
-    let span = document.querySelector(`#${spanId}`);
-    if (!span) {
-        span = document.createElement("span");
-        span.setAttribute("id", spanId);
-        span.setAttribute("class", "ErrorSpan");
-        span.textContent = errorMessage;
-    }
-
-    const padre = elemento.parentElement;
-
-    // Validar numero
-    if (regexNumero.test(elemento.value)) {
-        elemento.classList.remove("incorrecto");
-        elemento.classList.add("correcto");
-        if (span.parentNode === padre) {
-            padre.removeChild(span);
-        }
-    } else {
-        elemento.classList.remove("correcto");
-        elemento.classList.add("incorrecto");
-        if (span.parentNode !== padre) {
-            padre.appendChild(span);
-        }
-    }
-};
-
-
-
-
 
 const SoloLetras = function(event) {
     const regexLetras = /^[a-zA-Z\ ]{0,}$/;
@@ -134,13 +95,6 @@ const SoloLetras = function(event) {
         event.preventDefault();
     }
 }
-const SoloNumeros = function (event) {
-    const regexnums = /^[\d]{0,}$/;
-    if (!(regexnums.test(event.key))) {
-        event.preventDefault();
-        }
-}
-
 
 nombre.addEventListener("keypress",(event)=> SoloLetras(event))
 nombre.addEventListener("input",(event)=> cantidad(nombre))
@@ -155,13 +109,3 @@ Ndoc.addEventListener("input", () => validarNumeros(Ndoc))
 
 
 
-const toggleSubmitButton = () => {
-    if (terminos.checked) {
-        enviar.disabled = false;   
-    } else {
-        enviar.disabled = true;
-    }
-};
-
-terminos.addEventListener("change", toggleSubmitButton);
-toggleSubmitButton();
